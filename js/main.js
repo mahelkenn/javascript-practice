21ef03c3cc0ac6774e3aee6c50ec4547a75dc31c
Michele <mahelkenn@gmail.com>
2021-05-21 23:12:37 -0800

    Finished dropdown coding and changed margins/padding to match button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
 const button = document.querySelector(".button");
 const h2 = document.querySelectorAll("h2");
+const dropdown = document.querySelector(".dropdown");
+
 
 const menu = button.addEventListener("click", function () {
-    h2.classList.remove("hide");
+    if (dropdown.classList.contains("hide")) {
+        dropdown.classList.remove("hide");    
+    }
+    else {
+        dropdown.classList.add("hide");
+    };
 });
